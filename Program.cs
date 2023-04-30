186cce4501dfce83c41e3ed9e3e179044b0b5ba5
SheinShon <114719532+SheinShon@users.noreply.github.com>
2023-04-30 10:41:25 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculationLib;
+
+namespace Calculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Class1.Plus(10, 5));
+            Console.WriteLine(Class1.Minus(5, 8));
+            Console.WriteLine(Class1.Divide(42, 10));
+            Console.WriteLine(Class1.Multiply(3, 4));
+            Console.ReadLine();
+        }
+    }
+}
